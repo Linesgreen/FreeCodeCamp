40e67617e1227f25f0ed0a2359b6330ee081205f
Linesgreen <linesgreen@mail.ru>
2023-09-09 00:23:44 +0300

    FirstLine 	\SecondLine ThirdLine

---
+++ Escape_Sequences.js
@@ -0,0 +1,17 @@
+// Escape Sequences in Strings
+// Quotes are not the only characters that can be escaped inside a string.
+// Escape sequences allow you to use characters you may not otherwise be able to use in a string.
+// Способ форматирования текста внутри одной строки.
+/*  Code	Output
+        \'	single quote
+        \"	double quote
+        \\	backslash
+        \n	newline
+        \t	tab
+        \r	carriage return
+        \b	backspace
+        \f	form feed 
+*/
+
+const myStr = 'FirstLine\n\t\\SecondLine\nThirdLine'; // Change this line
+console.log(myStr);
