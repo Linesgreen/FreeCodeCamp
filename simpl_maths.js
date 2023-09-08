75c955be19c4b7e353c7efa342331fceeaaa5f1f
Linesgreen <linesgreen@mail.ru>
2023-09-08 20:10:27 +0300

    simple maths in js

---
+++ simpl_maths.js
@@ -0,0 +1,15 @@
+let a=2,
+    b=3,
+    summ,
+    subtract,
+    multiply,
+    divide;
+
+summ= a + b;
+subtract = a - b;
+multiply = a * b;
+divide = a / b;
+console.log(summ, 'сложение');
+console.log(subtract, 'вычитание');
+console.log(multiply, 'умножение');
+console.log(divide, 'деление');
