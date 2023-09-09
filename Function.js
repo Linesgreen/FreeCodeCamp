6e312d1b2a8e3f69846a82bb1bff50d8566db8fe
Linesgreen <linesgreen@mail.ru>
2023-09-09 19:17:24 +0300

    reusableFunction

---
+++ Function.js
@@ -0,0 +1,12 @@
+// Оборачиваем код в функцию
+
+
+function reusableFunction() {
+    console.log("Hi World");
+  }
+  
+
+// Вызываем функцию. Вместо того что бы писать весь код заново. 
+  reusableFunction();
+  reusableFunction();
+  reusableFunction();
