2d741824c2bce0990329cb2ac9f63ed902e58670
Linesgreen <linesgreen@mail.ru>
2023-09-10 16:37:22 +0300

    == - Знак равенства

---
+++ golf_score.js
@@ -0,0 +1,13 @@
+// Навсегда запомни что равенство в js это ==, а не =
+function golfScore(par, strokes) {
+
+  if (strokes == 1) {return "Hole-in-one!"}
+  else if (strokes == par + 2) {return "Double Bogey"}
+  else if (strokes == par + 1) {return "Bogey"}
+  else if (strokes == par) {return "Par"}
+    else if (strokes == par - 1) {return "Birdie"}
+    else if (strokes <= par - 2) {return "Eagle"}
+    else if (strokes >= par + 3) {return "Go Home!"}
+  }
+  
+console.log(golfScore(5,2));
