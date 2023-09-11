8e2c4c05d14d7580bb91afc74a9e3886585751a5
Linesgreen <linesgreen@mail.ru>
2023-09-11 21:02:36 +0300

    Accessing Nested Arrays

--- Accessing Nested Objects.js
+++ Accessing Nested Objects.js
@@ -14,3 +14,26 @@ const myStorage = {
   };
   
  console.log(myStorage.car.inside["glove box"]);
+
+ //////////////////////////////////////
+
+ const myPlants = [
+    {
+      type: "flowers",
+      list: [
+        "rose",
+        "tulip",
+        "dandelion"
+      ]
+    },
+    {
+      type: "trees",
+      list: [
+        "fir",
+        "pine",
+        "birch"
+      ]
+    }
+  ];
+  
+  console.log(myPlants[1].list[1]);
