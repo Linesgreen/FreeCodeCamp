066d777cf916b63741c60b2ca46696a76f8a1382
Linesgreen <linesgreen@mail.ru>
2023-09-11 20:53:01 +0300

    complex Objects

---
+++ Complex Objects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+     {
+      "artist": "Braebern",
+      "title": "Pony",
+      "release_year": 6969,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ]
+      
+    }
+  ];
