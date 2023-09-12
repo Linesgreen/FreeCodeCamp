937f22a6239ecd87bf6bff2deefaf2c666a00ef3
Linesgreen <linesgreen@mail.ru>
2023-09-12 18:53:46 +0300

    looooooooooooooooop

---
+++ cycle_for.js
@@ -0,0 +1,13 @@
+        /* for (a; b; c), where : a начальый параметр
+                                  b условие выполнения
+                                  c действие которое выполняется
+        */
+
+const myArray = [];
+
+for (let i = 1; i < 6; i++) {
+  myArray.push(i);
+  console.log(myArray);
+}
+// Only change code below this line
+
