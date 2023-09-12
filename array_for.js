07be4f0f5a2fc75ecc0fd603fd10bbd9b7e6d6da
Linesgreen <linesgreen@mail.ru>
2023-09-12 19:14:33 +0300

    sum array_for

---
+++ array_for.js
@@ -0,0 +1,15 @@
+// С помощью цикла просматриваем каждый элемент массива и находим всю сумму элементов в нем
+
+
+
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+for ( let i = 0, total = 0; i < myArr.length; i++) {
+      total = total + myArr[i];
+      
+      console.log(total);
+      
+}
+
+
+// Only change code below this line
