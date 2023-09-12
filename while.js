81209a59fecd907e90ff69a3fe4e35f7844e70f0
Linesgreen <linesgreen@mail.ru>
2023-09-12 18:39:28 +0300

    First while

---
+++ while.js
@@ -0,0 +1,14 @@
+// создаем цикл который выполняется пока не будет выполненно условие
+
+
+const myArray = [];
+let x = 0;
+
+console.log(myArray); 
+
+while (x<=5) {
+  myArray.push(x);  // Команда PUSHдобовляет значение в конец масива.
+  x++;
+}
+
+console.log(myArray);
