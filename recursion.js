db9b27ba9da07c062b7a0f6e4bde193dc03ae353
Linesgreen <linesgreen@mail.ru>
2023-09-13 21:28:12 +0300

    мерзкая рекурсия

---
+++ recursion.js
@@ -0,0 +1,14 @@
+
+function sum(arr, n) {
+    
+    if (n <= 0) {
+        return 0;
+      } else {
+        return arr[n-1]+sum(arr,n-1);
+      }
+    
+  }
+
+  
+  
+console.log(sum([2, 3, 4], 2));
