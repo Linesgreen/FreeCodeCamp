59bb6681a0fe0ed5a273817f057898cbbd6d6565
Linesgreen <linesgreen@mail.ru>
2023-09-18 19:23:41 +0300

    .match

---
+++ -.match.js
@@ -0,0 +1,10 @@
+// используя .match находим указанное слово в предложение и возвращаем само это слово 
+
+
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi // g - выводит все повторения в предложении, i - регистр не учитывается ;
+let result = twinkleStar; // Change this line
+
+result = twinkleStar.match(starRegex);
+console.log(result);
