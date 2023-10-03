c4dca61699ce32071f68d6b4aebd4d965869349a
Vladislav Evg <linesgreen@yandex.by>
2023-10-03 18:00:29 +0300

    v0.2

--- Nesting For Loops.js
+++ Nesting For Loops.js
@@ -1,22 +1,22 @@
 // работаем с вложенными массивами
 // Находим сумму всех значений во всех вложенных масивах в массив
 
-
-
 function multiplyAll(arr) {
-    let product = 0;
-    // Only change code below this line
-  for (let i = 0 ; i < arr.length; i++) {
-    for (let y = 0; y<arr[i].length; y++)
-    {
-    //   product = arr[i][y]+product;                            // Попытка дебага и поиска на какой стадии вылазила ошибка
-    //  console.log(arr[i][y]); console.log("--" + product);
-      
-      }
+  let product = 0;
+  // Only change code below this line
+  for (let i = 0; i < arr.length; i++) {
+    for (let y = 0; y < arr[i].length; y++) {
+      //   product = arr[i][y]+product;                            // Попытка дебага и поиска на какой стадии вылазила ошибка
+      //  console.log(arr[i][y]); console.log("--" + product);
+    }
   }
-    
-    console.log(product);
-    return product;
-  }
-  
-  multiplyAll([[1, 2], [3, 4], [5, 6, 7]]) 
+
+  console.log(product);
+  return product;
+}
+
+multiplyAll([
+  [1, 2],
+  [3, 4],
+  [5, 6, 7],
+]);
--- Switch Statements.js
+++ Switch Statements.js
@@ -1,28 +1,31 @@
 // В зависимости от переданого значения в функцию срабатывает определенная строка (case)
 // break в конце нужен для прерывания выполнения функции.
 
-
 function caseInSwitch(val) {
-    let answer = "";
-    // Only change code below this line
-    switch (val) {
-      case 1: answer = "alpha";
-        break;
-      case 2: answer = "beta";
-        break;
-      case 3: answer = "gamma";
-        break;
-      case 4: answer = "delta";
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
 
-// default используется для значений которые не подошли под case которые я указал.
+    // default используется для значений которые не подошли под case которые я указал.
 
-      default: answer = "another";
-    }
-  
-  
-    // Only change code above this line
-    return answer;
+    default:
+      answer = "another";
   }
-  
-  console.log(caseInSwitch(1));
-  console.log(caseInSwitch(2));
+
+  // Only change code above this line
+  return answer;
+}
+
+console.log(caseInSwitch(1));
+console.log(caseInSwitch(2));
